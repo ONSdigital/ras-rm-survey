f772b3165f2a34d7d627506a62e996e2096e8f79
Matt <matt.crocker@gmail.com>
2020-10-02 16:50:30 +0100

    Postgres schemas and GORM aren't friends

--- main.go
+++ main.go
@@ -16,11 +16,14 @@ func main() {
 
 	log.Println("Starting application...")
 
-	dbURI := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable search_path=%s", viper.GetString("db_host"), viper.GetString("db_port"), viper.GetString("db_name"), viper.GetString("db_username"), viper.GetString("db_password"), viper.GetString("db_schema"))
+	dbURI := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", viper.GetString("db_host"), viper.GetString("db_port"), viper.GetString("db_name"), viper.GetString("db_username"), viper.GetString("db_password"))
 	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
 	if err != nil {
 		log.Fatalln("Couldn't connect to postgres, " + err.Error())
 	}
+
+	db.Exec(fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS %s;", viper.GetString("db_schema")))
+	db.Exec(fmt.Sprintf("SET search_path TO %s;", viper.GetString("db_schema")))
 
 	db.AutoMigrate(&Survey{}, &CollectionExercise{}, &CollectionInstrument{}, &Email{})
 
